1c39a305476f64a24b4fee0600d54ed734383c5f
lvinia <l.lavinia@escolar.ifrn.edu.br>
2023-10-11 10:35:03 -0300

    fase1

---
+++ Jogo da bolinha/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int velocidade = 10;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("START");
+        TryGetComponent(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+
+        if (transform.position.y <= -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
